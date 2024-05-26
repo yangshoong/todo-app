65863bfdb9020d36f2a1687e063342cb4e44708e
yangshoong <crinaeae83@gmail.com>
2024-05-27 03:50:06 +0900

    console.log

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const express = require('express');
+console.log("1");
 const mongoose = require('mongoose');
-const cors = require('cors')
+console.log("2");
+const cors = require('cors');
+console.log("3");
 const bodyParser = require('body-parser');
+console.log("4");
 const indexRouter = require('./routes/index');
+console.log("5");
 require('dotenv').config();
+console.log("6");
 const app = express();
+console.log("7");
 const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
+console.log("8");
 app.use(cors());
+console.log("9");
 app.use(bodyParser.json());
+console.log("10");
 app.use("/api", indexRouter);
+console.log("11");
 
 const mongoURI = MONGODB_URI_PROD;
+console.log("12");
 
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
