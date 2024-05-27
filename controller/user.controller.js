ca88c52a97db3dffb0357fba11ee35d48f9cccde
yangshoong <crinaeae83@gmail.com>
2024-05-28 03:16:31 +0900

    bcryptjs

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,5 +1,5 @@
 const User = require("../model/User");
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
 const saltRounds = 10;
 const userController = {};
 
--- model/User.js
+++ model/User.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 require('dotenv').config();
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
