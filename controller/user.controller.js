6407a88b120f82cb1358661377f6844572165173
yangshoong <crinaeae83@gmail.com>
2024-05-28 02:55:17 +0900

    로그인 fe

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -38,7 +38,7 @@ userController.loginWithEmail = async (req, res) => {
     }
     throw new Error("아이디 또는 비밀번호가 일치하지 않습니다");
   } catch (error) {
-    res.status(400).json({ status: "fail", error: error.message });
+    res.status(400).json({ status: "fail", message: error.message });
   }
 };
 
