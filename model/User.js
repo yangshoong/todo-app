cee12effe9cd883f0b76d2c4b0d7543337ac343a
yangshoong <crinaeae83@gmail.com>
2024-05-28 01:58:53 +0900

    로그인 기능 be

--- model/User.js
+++ model/User.js
@@ -1,5 +1,9 @@
-const mongoose = require("mongoose")
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+
 const userSchema = Schema({
   name: {
     type: String,
@@ -13,7 +17,19 @@ const userSchema = Schema({
     type: String,
     required: true
   }
-}, { timestamps: true })
+}, { timestamps: true });
+
+userSchema.methods.toJSON = function(){
+  const obj = this._doc;
+  delete obj.password;
+  return obj;
+};
+
+userSchema.methods.generateToken = function() {
+  const token = jwt.sign({ _id: this._id }, JWT_SECRET_KEY,{expiresIn:'1d'});
+  return token;
+};
+
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
--- routes/user.api.js
+++ routes/user.api.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const userController = require('../controller/user.controller');
 
 router.post("/", userController.createUser);
-router.post("/login", userController);
+router.post("/login", userController.loginWithEmail);
 
 module.exports = router;
